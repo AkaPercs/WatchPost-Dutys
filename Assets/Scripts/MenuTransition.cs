b6f912f828fb305b948607b41d0308f922965999
Guillermo Belloso <bellosoguillermo@yahoo.com>
2023-11-29 20:05:40 -0800

    new stuff
    newest edition 11/29/2023

--- Assets/Scripts/MenuTransition.cs
+++ Assets/Scripts/MenuTransition.cs
@@ -6,6 +6,6 @@ public class MainSceneLoader : MonoBehaviour
     // Public method to load the "Hex" scene
     public void LoadMainScene()
     {
-        SceneManager.LoadScene("NewMain");
+        SceneManager.LoadScene("Main");
     }
 }
